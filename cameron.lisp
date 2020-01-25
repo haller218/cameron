b75e13f74f1d576d544a29ca2f1ea350d61f2cc6
haller218 <eliseu42lucena@gmail.com>
2020-01-25 20:34:12 -0300

    begin project stable work

--- cameron.lisp
+++ cameron.lisp
@@ -1,5 +1,3 @@
-#!/usr/bin/sbcl --script
-
 (require "cl-telegram-bot")
 
 (defpackage the-bot (:use :cl :cl-telegram-bot))
@@ -13,17 +11,43 @@
   (reply text))
 
 (defmethod on-command ((bot echo-bot)
-		       (command (eql :help))
+		       (command (eql :list))
 		       text)
   (declare (ignorable text))
+  (reply "/help /whoami /run /eval"))
+
+(defmethod on-command ((bot echo-bot)
+		       (command (eql :help))
+		       text)
+  (declare (ignorable text))]
   (reply "Just send me any text and I'll reply with the same text."))
 
+(defmethod on-command ((bot echo-bot)
+		       (command (eql :eval))
+		       text)
+  (declare (ignorable text))
+  (reply (eval (read-from-string text))))
+
+(defmethod on-command ((bot echo-bot)
+		       (command (eql :run))
+		       text)
+  (declare (ignorable text))
+  (reply "run"))
+
+
+(defmethod on-command ((bot echo-bot)
+		       (command (eql :whoami))
+		       text)
+  (declare (ignorable text))
+  (reply (uiop:hostname)))
+ 
 
 (defmethod on-command ((bot echo-bot)
 		       (command (eql :start))
 		       text)
   (declare (ignorable text))
   (reply "Wellcome Lisper! Have a fun, playing with cl-telegram-bot!"))
+
 
 
 (start-processing (make-echo-bot (uiop:getenv "API_TELEGRAM_BOTNET"))
