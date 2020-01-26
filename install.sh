5de1637663fa354159c817d1774a939cb20e8114
haller218 <eliseu42lucena@gmail.com>
2020-01-26 10:04:24 -0300

    some output log

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+echo ">>> CURL QUICKLISP:"
+
 curl -O https://beta.quicklisp.org/quicklisp.lisp
+
+echo ">>> INSTALL QUICKLISP:"
 
 sbcl --load quicklisp.lisp --eval "(quicklisp-quickstart:install)(ql:add-to-init-file)"
 
 cd quicklisp/local-projects
 
+echo ">>> INSTALL cl-telegram-bot:"
+
 git clone https://github.com/40ants/cl-telegram-bot.git
 
 cp -Ra cl-telegram-bot $HOME/quicklisp/local-projects
+
+echo ">>> INSTALL PACKAGES:"
 
 sbcl --eval "(ql:quickload \"cl-telegram-bot/core\")"
 
