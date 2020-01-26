af762e6959bbad00fd536c59ed719cf4cfbc0d12
haller218 <eliseu42lucena@gmail.com>
2020-01-26 14:06:54 +0000

    some changes for install

--- install.sh
+++ install.sh
@@ -2,15 +2,23 @@
 
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 
-sbcl --load quicklisp.lisp --eval "(quicklisp-quickstart:install)(ql:add-to-init-file)"
+sbcl --load quicklisp.lisp --eval "(quicklisp-quickstart:install)^D"
 
-cd quicklisp/local-projects
+rm quicklisp.lisp
+
+sbcl --load /home/$USER/quicklisp/setup.lisp --eval "(ql:add-to-init-file)^D"
+
+cd $HOME/quicklisp/local-projects
 
 git clone https://github.com/40ants/cl-telegram-bot.git
 
 cp -Ra cl-telegram-bot $HOME/quicklisp/local-projects
 
+cd -
+
 sbcl --eval "(ql:quickload \"cl-telegram-bot/core\")"
+
+
 
 
 
